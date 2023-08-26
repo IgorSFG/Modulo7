eb5fc7bfa57fc10d9f11ec81e2ed65b808974dea
IgorSFG <igor.garcia@sou.inteli.edu.br>
2023-08-26 18:46:56 -0300

    postgres funcionando!

--- pond2/backend/index.js
+++ pond2/backend/index.js
@@ -1,8 +1,8 @@
-const { Pool } = require('pg');
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const jwt = require('jsonwebtoken');
 const path = require('path');
+const db = require('./db');
 
 const app = express();
 const PORT = 3000;
@@ -12,38 +12,10 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../frontend')));
 
-const client = new Pool({
-  user: 'postgres',
-  host: '127.0.0.1',
-  database: 'todoapp',
-  password: 'postgres',
-  port: 5432,
-});
-
-async function createUserTable(client) {
-  const userTable = `CREATE TABLE IF NOT EXISTS Users (
-    id SERIAL PRIMARY KEY,
-    usr VARCHAR(50) UNIQUE NOT NULL,
-    pwd VARCHAR(50) NOT NULL
-  )`;
-
-  try {
-    await client.query(userTable);
-    console.log('Created "Users" table');
-  } catch (error) {
-    console.error('Error creating "Users" table:', error);
-    throw error;
-  }
-
-  // const insertTest = `INSERT INTO Users (usr, pwd) VALUES ($1, $2)`;
-  // await client.query(insertTest, ['test', 'test123']);
-  // console.log('Test user inserted');
-}
-
 
 // Generate JWT token for a user
 function generateToken(user) {
-  return jwt.sign({ id: user.id }, secretKey, { expiresIn: '1h' });
+  return jwt.sign({ user_id: user.user_id, username: user.username, password: user.password }, secretKey, { expiresIn: '1h' });
 }
 
 // Middleware to authenticate token
@@ -64,7 +36,8 @@ function authenticateToken(req, res, next) {
 }
 
 app.get('/', async (req, res) => {
-  createUserTable(client);
+  //db.createUserTable();
+  //db.insertUser();
   const filePath = path.join(__dirname, '../frontend/pages', 'login.html');
   res.sendFile(filePath);
 });
@@ -73,15 +46,14 @@ app.post('/login', async (req, res) => {
   console.log(req.body);
   const { username, password } = req.body;
   
-  // Query the database to check if the user exists
-  const query = `SELECT * FROM Users WHERE usr = $1 AND pwd = $2`;
-  const { rows } = await client.query(query, [username, password]);
+  const rows = await db.getUser(username, password);
 
   if (rows.length === 0) {
     return res.sendStatus(404);
   }
 
   const user = rows[0];
+  console.log(user);
   const token = generateToken(user);
   res.cookie('token', token, { httpOnly: true });
   res.json({ token });
